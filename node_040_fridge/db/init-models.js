b15a14fc73ed386ffd72a99222abede4e559c982
hykim6856 <hykim6856@naver.com>
2024-02-08 15:10:55 +0900

    upload

--- node_040_fridge/db/init-models.js
+++ node_040_fridge/db/init-models.js
@@ -1,10 +1,9 @@
 import _sequelize from "sequelize";
 const DataTypes = _sequelize.DataTypes;
-import _tbl_product from  "./tbl_product.js";
+import _tbl_product from "../models/tbl_product.js";
 
 export default function initModels(sequelize) {
   const tbl_product = _tbl_product.init(sequelize, DataTypes);
-
 
   return {
     tbl_product,
