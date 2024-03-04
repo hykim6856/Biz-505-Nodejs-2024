22889c5d6423ca87cac2d084b4c2c35e06a87ced
hykim6856 <hykim6856@naver.com>
2024-03-04 16:11:27 +0900

    up

---
+++ node_060_books/config/naver_secret.js
@@ -0,0 +1,5 @@
+export const NAVER_CLIENT_ID = "bd5Xbx0aQzoWwwVY5W__";
+export const NAVER_SECRET = "YDymafWk4S";
+export const NAVER_BOOK_URL =
+  "https://openapi.naver.com/v1/search/book.json";
+// export { NAVER_BOOK_URL, NAVER_SECRET, NAVER_CLIENT_ID };
---
+++ node_060_books/config/naver_secret_sample.js
@@ -0,0 +1,11 @@
+/**
+ * 네이버 개발자 센터에 애플리케이션을 등록하고 ID 와 SECRET 를 확인 한 후
+ * 아래 변수에 설정하고 이 파일을 naver_secret.js 로 이름 변경한 후
+ * 프로젝트를 실행할 것
+ */
+
+const NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+const NAVER_SECRET = "YOUR NAVER CLIENT SECRET";
+const NAVER_BOOK_URL =
+  "https://openapi.naver.com/v1/search/book.json";
+// export { NAVER_BOOK_URL, NAVER_SECRET, NAVER_CLIENT_ID };
