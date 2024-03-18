cc12deac3a9a966a7f75aa69c1c957d046ce8e56
hykim6856 <hykim6856@naver.com>
2024-03-18 16:43:12 +0900

    검색

--- oneday_school_db/routes/index.js
+++ oneday_school_db/routes/index.js
@@ -18,6 +18,9 @@ router.get("/", async (req, res, next) => {
 router.get("/detail/:s_code", async (req, res, next) => {
   const s_code = req.params.s_code;
   const students = await STUDENT.findAll();
+  const subjects = await SUBJECT.findAll({
+    order: [["n_code", "ASC"]],
+  });
   const studentDetail = await STUDENT.findAll({
     where: { s_code: s_code },
   });
@@ -25,6 +28,28 @@ router.get("/detail/:s_code", async (req, res, next) => {
   res.render("index", {
     title: "학생 성적 처리 프로젝트",
     STUDENTS: students,
+    SUBJECTS: subjects,
+    STUDENTDETAIL: studentDetail,
+    TESTS: tests,
+  });
+});
+
+router.post("/detail", async (req, res, next) => {
+  const s_name = req.body.s_name;
+  const students = await STUDENT.findAll();
+  const subjects = await SUBJECT.findAll({
+    order: [["n_code", "ASC"]],
+  });
+  const studentDetail = await STUDENT.findAll({
+    where: { s_name: s_name },
+  });
+  const tests = await TEST.findAll({
+    where: { t_scode: studentDetail.s_code },
+  });
+  res.render("index", {
+    title: "학생 성적 처리 프로젝트",
+    STUDENTS: students,
+    SUBJECTS: subjects,
     STUDENTDETAIL: studentDetail,
     TESTS: tests,
   });
